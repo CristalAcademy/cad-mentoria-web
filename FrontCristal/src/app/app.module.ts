a7d87f01045ada97855543f9702555620cbcc004
Stefanie Silva <stefanie.souza@cxmail.com.br>
2022-01-11 15:51:34 -0300

    Modelagem de projeto

--- FrontCristal/src/app/app.module.ts
+++ FrontCristal/src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Material } from './material.module'
+import { Material } from '../assets/module/material.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { CadastroComponent } from './componentes/cadastro/cadastro.component';
+import { AcessoComponent } from './views/acesso/acesso.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    AcessoComponent
   ],
   imports: [
     BrowserModule,
