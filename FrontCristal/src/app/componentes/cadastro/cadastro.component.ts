3b1b904631c82b126b92bb6c65a964d2c68bd367
Bruno Vidal <bruno.vidal@opah.com.br>
2022-01-12 18:23:26 -0300

    Add browser module

--- FrontCristal/src/app/componentes/cadastro/cadastro.component.ts
+++ FrontCristal/src/app/componentes/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl,  NgForm,  Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormControl, NgForm, Validators, FormGroup } from '@angular/forms';
 import { CadastroCandidatoService } from 'src/app/services/cadastro-candidato.service';
 
 
@@ -8,14 +9,26 @@ import { CadastroCandidatoService } from 'src/app/services/cadastro-candidato.se
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.scss']
 })
-export class CadastroComponent  {
+export class CadastroComponent implements OnInit  {
+  constructor(private formBd: FormBuilder) {}
 
-  nomeFormControl = new FormControl('', [Validators.required]);
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  passwordFormControl = new FormControl('', [Validators.required]);
-  passwordCFormControl = new FormControl('', [Validators.required]);
+  form!: FormGroup;
 
   public hide : boolean = true;
+
+  ngOnInit() {
+    this.form = this.formBd.group({
+      nome : this.formBd.control('', [Validators.required]),
+      email : this.formBd.control('', [Validators.required, Validators.email]),
+      password: this.formBd.control('', [Validators.required]),
+      passwordC : this.formBd.control('', [Validators.required])
+    })
+
+    this.form.valueChanges.subscribe(change => {
+      console.log(change)
+    })
+
+  }
 
   onSubmit(form: NgForm) {
 
