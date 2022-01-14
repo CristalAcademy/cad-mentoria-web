585ab2f4987740a47992aeacbfecc32ea2508621
Stefanie Silva <stefanie.souza@cxmail.com.br>
2022-01-14 13:16:12 -0300

    Mat-form-field add em login

--- FrontCristal/src/app/app.module.ts
+++ FrontCristal/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { Material } from '../assets/module/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -13,7 +13,7 @@ import { TooltipComponent } from './componentes/tooltip/tooltip.component';
 import { CadastroComplementarComponent } from './componentes/cadastro/cadastro-candidato/cadastro-candidato.component';
 import { CadastroCandidatoSocialComponent } from './componentes/cadastro/cadastro-candidato-social/cadastro-candidato-social.component';
 import { StatusCandidaturaComponent } from './componentes/status-candidatura/status-candidatura.component';
-import { EsqueciMinhaSenhaComponent } from './componentes/esqueci-minha-senha/esqueci-minha-senha.component';
+import { EsqueciMinhaSenhaComponent } from './componentes/login/esqueci-minha-senha/esqueci-minha-senha.component';
 
 
 @NgModule({
--- FrontCristal/src/app/componentes/login/login.component.ts
+++ FrontCristal/src/app/componentes/login/login.component.ts
@@ -1,4 +1,11 @@
+import { LoginService } from './../../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +15,45 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
   senha!: string;
   senhaErrada: boolean = false;
-  constructor() {}
+  f = FormControl;
+  constructor(private formBd: FormBuilder) {}
 
-  ngOnInit(): void {}
+  form!: FormGroup;
+
+  public hide: boolean = true;
+
+  ngOnInit(): void {
+    this.form = this.formBd.group({
+      email: this.formBd.control('', [Validators.required, Validators.email]),
+      password: this.formBd.control('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(16),
+      ]),
+    });
+  }
   verificarSenha() {
     this.senhaErrada = this.senha.length < 8;
+  }
+  onSubmit() {
+    let login = new LoginService(
+      this.form.value.email,
+      this.form.value.password
+    );
+
+    console.log(login);
+  }
+
+  getErrorMessage(prop: string) {
+
+    if (this.element(prop)!.hasError('required')) {
+      return 'Campo não pode ser vazio';
+    }else  if (this.element(prop)!.hasError('email')) {
+      return 'Email não está válido';
+    }
+    return 'Senha deve ter ao menos de 8 caracteres';
+  }
+  element(key : string) {
+    return this.form.get(key);
   }
 }
