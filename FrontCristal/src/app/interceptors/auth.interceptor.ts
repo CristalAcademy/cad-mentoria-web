82b1e60829392ffc3f44e0b37f51fc0618afe0b4
Stefanie Silva <stefanie.souza@cxmail.com.br>
2022-02-04 13:00:10 -0300

    Correção de token

--- FrontCristal/src/app/interceptors/auth.interceptor.ts
+++ FrontCristal/src/app/interceptors/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable()
@@ -12,11 +13,19 @@ export class AuthInterceptor implements HttpInterceptor {
       email: string;
     } = JSON.parse(localStorage.getItem('login') ||'');
 
+
+    if (req.url != `${environment.path}/authenticate`) {
+      req = req.clone({
+        setHeaders: {
+          'Authorization': `Bearer ${token.token}`,
+        },
+      });
+    }
+
     req = req.clone({
       setHeaders: {
         'Content-Type' : 'application/json; charset=utf-8',
         'Accept'       : 'application/json',
-        'Authorization': `Bearer ${token.token}`,
       },
     });
 
