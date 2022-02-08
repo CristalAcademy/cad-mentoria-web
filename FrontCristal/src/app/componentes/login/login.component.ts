6bd3521e8a6d50ba8805eb01e51e2c079c90f996
CristalAcademy <cristalacademysdg@gmail.com>
2022-02-08 13:45:16 -0300

    Correção do bug de login

--- FrontCristal/src/app/componentes/login/login.component.ts
+++ FrontCristal/src/app/componentes/login/login.component.ts
@@ -46,9 +46,9 @@ export class LoginComponent implements OnInit {
   }
   onSubmit() {
     let login = new LoginModel(this.form.value.email, this.form.value.password);
+    console.log(login);
 
     this.loginService.logar(login);
-    
   }
 
   getErrorMessage(prop: string): string {
--- FrontCristal/src/app/interceptors/auth.interceptor.ts
+++ FrontCristal/src/app/interceptors/auth.interceptor.ts
@@ -10,14 +10,19 @@ import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    var token: {
-      token: string;
-      email: string;
-    } = JSON.parse(localStorage.getItem('login') || '');
-
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     if (
-      req.url != `${environment.path}/authenticate` && req.url != `${environment.path}/recuperar-senha` && req.url != `${environment.path}/recuperar-senha/confirmar`) {
+      req.url != `${environment.path}/authenticate` &&
+      req.url != `${environment.path}/recuperar-senha` &&
+      req.url != `${environment.path}/recuperar-senha/confirmar`
+    ) {
+      var token: {
+        token: string;
+        email: string;
+      } = JSON.parse(localStorage.getItem('login') || '');
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token.token}`,
