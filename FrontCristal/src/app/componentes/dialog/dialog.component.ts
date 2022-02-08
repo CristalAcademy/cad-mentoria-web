3a12fd26e15491a3d95a541260c0795cc51b3688
Stefanie Silva <stefanie.souza@cxmail.com.br>
2022-02-08 10:34:39 -0300

    Integração de rotas multiplas e menu auto-gerenciado

--- FrontCristal/src/app/componentes/dialog/dialog.component.ts
+++ FrontCristal/src/app/componentes/dialog/dialog.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
   styleUrls: ['./dialog.component.scss'],
 })
-export class DialogComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class DialogComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    public router: Router
+  ) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+    this.router.navigate(['/login']);
+  }
 }
